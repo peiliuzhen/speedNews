7fcef9db35cf67cd2784f0e668489508a95ec590
Medalands <medalands@172-5-0-104.lightspeed.oshkwi.sbcglobal.net>
2015-10-23 09:47:46 +0800

    Initial Commit

---
+++ MDSpeedNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MDSpeedNews
+//
+//  Created by Medalands on 15/10/23.
+//  Copyright (c) 2015年 Medalands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MDSpeedNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MDSpeedNews
+//
+//  Created by Medalands on 15/10/23.
+//  Copyright (c) 2015年 Medalands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
